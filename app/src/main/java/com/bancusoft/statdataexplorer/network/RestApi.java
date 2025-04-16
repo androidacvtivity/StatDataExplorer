feeab94ee1961aeef860c1db94331233c3d5237a
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-04-16 10:45:15 +0300

    Show filtered results in list

--- app/src/main/java/com/bancusoft/statdataexplorer/network/RestApi.java
+++ app/src/main/java/com/bancusoft/statdataexplorer/network/RestApi.java
@@ -2,9 +2,21 @@ package com.bancusoft.statdataexplorer.network;
 
 import com.bancusoft.statdataexplorer.models.ResponseModel;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface RestApi {
     @GET("index_view.php")
     Call<ResponseModel> getViewData();
+
+    @FormUrlEncoded
+    @POST("index_view.php")
+    Call<ResponseModel> searchCompanies(
+            @Field("actionvw") String action,
+            @Field("queryvw") String query,
+            @Field("limitvw") int limit,
+            @Field("startvw") int start
+    );
 }
