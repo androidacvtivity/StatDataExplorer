935259b85d931a5a5f14690c8e5a8f88620f5cc1
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-05-16 14:01:06 +0300

    versionCode 22

---
+++ app/src/main/java/com/bancusoft/statdataexplorer/network/RetrofitClient.java
@@ -0,0 +1,29 @@
+package com.bancusoft.statdataexplorer.network;
+
+import java.util.concurrent.TimeUnit;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static final String BASE_URL = "http://bancusoft.com/PHP/production/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(60, TimeUnit.SECONDS)
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .writeTimeout(60, TimeUnit.SECONDS)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
