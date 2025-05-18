168c000231b95b57b40b6951072a47f4fc979b12
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-05-18 19:12:22 +0300

    versionCode 24

--- app/src/main/java/com/bancusoft/statdataexplorer/activities/EmployeeMenuActivity.java
+++ app/src/main/java/com/bancusoft/statdataexplorer/activities/EmployeeMenuActivity.java
@@ -2,7 +2,7 @@ package com.bancusoft.statdataexplorer.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,28 +10,25 @@ import com.bancusoft.statdataexplorer.R;
 
 public class EmployeeMenuActivity extends AppCompatActivity {
 
-    Button btnAlpha, btnStar, btnDepart, btnGalaxy;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_menu);
 
-        btnAlpha = findViewById(R.id.btnAlpha);
-        btnStar = findViewById(R.id.btnStar);
-        btnDepart = findViewById(R.id.btnDepart);
-        btnGalaxy = findViewById(R.id.btnGalaxy);
+        findViewById(R.id.cardAlpha).setOnClickListener(v ->
+                startActivity(new Intent(this, EmployeesListActivity.class))
+        );
 
-        btnAlpha.setOnClickListener(v ->
-                startActivity(new Intent(this, EmployeesListActivity.class)));
-
-//        btnStar.setOnClickListener(v ->
-//                startActivity(new Intent(this, EmployeesByStarActivity.class)));
+//        findViewById(R.id.cardStar).setOnClickListener(v ->
+//                startActivity(new Intent(this, EmployeesByStarActivity.class))
+//        );
 //
-//        btnDepart.setOnClickListener(v ->
-//                startActivity(new Intent(this, EmployeesByDepartActivity.class)));
+//        findViewById(R.id.cardDepart).setOnClickListener(v ->
+//                startActivity(new Intent(this, EmployeesByDepartActivity.class))
+//        );
 //
-//        btnGalaxy.setOnClickListener(v ->
-//                startActivity(new Intent(this, EmployeesByGalaxyActivity.class)));
+//        findViewById(R.id.cardGalaxy).setOnClickListener(v ->
+//                startActivity(new Intent(this, EmployeesByGalaxyActivity.class))
+//        );
     }
 }
